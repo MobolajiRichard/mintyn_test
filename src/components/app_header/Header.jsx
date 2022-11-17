5b46990bf782bcca23c1d6a502f54359fa762573
Mobolaji Richard <104364699+MobolajiRichard@users.noreply.github.com>
2022-11-17 21:58:24 +0100

    header created

---
+++ src/components/app_header/Header.jsx
@@ -0,0 +1,33 @@
+import "./header.css";
+import { Avatar, Badge } from "@mui/material";
+import { NotificationsOutlined, Search } from "@mui/icons-material";
+
+const Header = () => {
+  return (
+    <header className="header__container">
+      <h3>TransMonitor</h3>
+      <div className="header__search__container">
+        <Search />
+        <input className="header__search" placeholder="Search..." />
+      </div>
+      <nav className="header__nav">
+        <a>Support</a>
+        <a>FAQS</a>
+        <div className="header__nav__notification">
+          <Badge badgeContent={2} overlap="circular" color="primary">
+            <NotificationsOutlined  />
+          </Badge>
+        </div>
+        <div className="header__nav__avatar">
+          <div className="header__nav__avatar__intro">
+            <p className="header__nav__avatar__hello">Hello</p>
+            <p className="header__nav__avatar__name">Oginni Mobolaji Richard</p>
+          </div>
+          <Avatar />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
