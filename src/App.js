ddb1fcc14d9444447a622c28a3ba3691e18422e9
Mobolaji Richard <104364699+MobolajiRichard@users.noreply.github.com>
2022-11-18 03:23:32 +0100

    sidebar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Header from "./components/app_header/Header"
+import Sidebar from "./components/app_sidebar/Sidebar"
+import Main from "./components/app_main/Main"
+import './App.css'
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <div className="body__container">
+        <div className="sidebar">
+        <Sidebar/>
+        </div>
+        <div className="main">
+        <Main/>
+        </div>
+      </div>
     </div>
   )
 }
